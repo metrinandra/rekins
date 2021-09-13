dcdd8f2a5283e430a8e638723109118cd30258d4
Andra <andra.metrina@gmail.com>
2021-09-13 13:22:48 +0300

    Turpinat, neturpinat no main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import rekins
 
 
+
 def print_info():
     # Programmu palaižot,
     # uz ekrāna tiek izvadīts īss apraksts par programmas darbību.
@@ -12,7 +13,7 @@ def lietotaja_ievade():
 # veltījuma tekstu (simbolu virkni);
 # lādītes izmēru – platumu, garumu un augstumu milimetros (veselus skaitļus);
 # kokmateriāla cenu EUR/m 2 .
-    vards = input('Ieraksti savu vardu: ')
+    vards = input('Ieraksti vardu: ')
     veltijuma_teksts = input('Ievadi veltijuma tekstu kastitei: ')
     platums = input('Cik plata kastite? ***Noraadi milimetrus. Tikai veseli skaitli!***')
     garums = input('Cik  plata garumaa? ***Noraadi milimetrus.Tikai veseli skaitli!***')
@@ -24,8 +25,14 @@ def lietotaja_ievade():
 # Programmas rekins main()
 if __name__ == '__main__':
     print_info()
-    while 1>0:
+    while True:
        lietotaja_ievade()
-       rekins = rekins.Rekins()
+      # rekins = rekins.Rekins()
        run_again = input('Vai velies sagatavot rekinu velreiz? JAA - 1, NEE - 0')
+       if run_again == 1:
+           continue
+       else:
+           exit(0)
+
+
 
