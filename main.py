9f681395f3d3114949d9cfe69e4cada0cb949ca9
Andra <andra.metrina@gmail.com>
2021-09-13 13:09:47 +0300

    Rekina programmas setup

---
+++ main.py
@@ -0,0 +1,31 @@
+import rekins
+
+
+def print_info():
+    # Programmu palaižot,
+    # uz ekrāna tiek izvadīts īss apraksts par programmas darbību.
+    print('Programma rekina sagatavosanai')
+
+def lietotaja_ievade():
+# User input:
+# klienta vārdu (simbolu virkni);
+# veltījuma tekstu (simbolu virkni);
+# lādītes izmēru – platumu, garumu un augstumu milimetros (veselus skaitļus);
+# kokmateriāla cenu EUR/m 2 .
+    vards = input('Ieraksti savu vardu: ')
+    veltijuma_teksts = input('Ievadi veltijuma tekstu kastitei: ')
+    platums = input('Cik plata kastite? ***Noraadi milimetrus. Tikai veseli skaitli!***')
+    garums = input('Cik  plata garumaa? ***Noraadi milimetrus.Tikai veseli skaitli!***')
+    augstums = input('Cik  augsta? ***Noraadi milimetrus.Tikai veseli skaitli!***')
+    kokmateriala_cena = input('Kokmateriala cena')
+    return vards, veltijuma_teksts, platums, garums, augstums, kokmateriala_cena
+
+
+# Programmas rekins main()
+if __name__ == '__main__':
+    print_info()
+    while 1>0:
+       lietotaja_ievade()
+       rekins = rekins.Rekins()
+       run_again = input('Vai velies sagatavot rekinu velreiz? JAA - 1, NEE - 0')
+
---
+++ rekins.py
@@ -0,0 +1,9 @@
+class Rekins:
+
+    def __init__(self,vards, teksts, garums, platums,augstums, materials):
+        self.vards = vards
+        self.teksts = teksts
+        self.garums =garums
+        self.platums = platums
+        self.augstum = augstums
+        self.materials = materials
